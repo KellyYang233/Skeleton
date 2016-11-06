001c0fc64fd090e7f0c3d1064987a9f794b146fc
Michael Wisslead <michael.wisslead@gmail.com>
2016-11-06 08:37:33 -0600

    Cleaned up code and reduced memory copies.

--- src/skeletonize.cpp
+++ src/skeletonize.cpp
@@ -4,22 +4,30 @@
 namespace cv
 {
 
+static void expandBorder(InputOutputArray _src)
+{
+    Mat src = Mat::zeros(_src.size() + Size(2, 2), _src.type());
+    _src.getMat().copyTo(src(Rect(1, 1, _src.size().width, _src.size().height)));
+    _src.assign(src);
+}
+
+static void removeBorder(InputOutputArray _src)
+{
+    Mat src;
+    _src.getMat()(Rect(1, 1, _src.size().width-2, _src.size().height-2)).copyTo(src);
+    _src.assign(src);
+}
+
+
 static int applyLUTNeighbors(InputArray _src, OutputArray _dst, uchar *lut)
 {
     Mat src = _src.getMat();
+    Mat dst = _dst.getMat();
     CV_Assert(src.type() == CV_8UC1);
     CV_Assert(src.channels() == 1);
-    _dst.create(src.size(), src.type());
-    Mat dst = _dst.getMat();
     int numChanges = 0;
-    uchar *dstptr = dst.ptr<uchar>(0);
-    for(int j = 0; j < src.cols; j++){
-        dstptr[j] = 0;
-    }
     for(int i = 1; i < src.rows-1; i++){
-        dstptr = dst.ptr<uchar>(i);
-        dstptr[0] = 0;
-        dstptr[src.cols-1] = 0;
+        uchar *dstptr = dst.ptr<uchar>(i);
         uchar *up = src.ptr<uchar>(i-1);
         uchar *cur = src.ptr<uchar>(i);
         uchar *down = src.ptr<uchar>(i+1);
@@ -37,12 +45,35 @@ static int applyLUTNeighbors(InputArray _src, OutputArray _dst, uchar *lut)
             }
         }
     }
-    dstptr = dst.ptr<uchar>(src.rows-1);
-    for(int j = 0; j < src.cols; j++){
-        dstptr[j] = 0;
-    }
     return numChanges;
 }
+
+static void simpleLut(InputArray _src, OutputArray _dst, uchar *lut)
+{
+    Mat src;
+    threshold(_src, src, 0, 1, THRESH_BINARY);
+    src.convertTo(src, CV_8UC1);
+    expandBorder(src);
+    Mat dst(src.size(), src.type());
+    applyLUTNeighbors(src, dst, lut);
+    removeBorder(dst);
+    _dst.assign(dst);
+}
+
+static void twoPassThinning(InputArray _src, OutputArray _dst, uchar *lut1, uchar *lut2)
+{
+    Mat src;
+    threshold(_src, src, 0, 1, THRESH_BINARY);
+    src.convertTo(src, CV_8UC1);
+    expandBorder(src);
+    Mat dst(src.size(), src.type());
+
+    while(applyLUTNeighbors(src, dst, lut1) + applyLUTNeighbors(dst, src, lut2));
+
+    removeBorder(src);
+    _dst.assign(src);
+}
+
 
 static void morphologicalThinning(InputArray _src, OutputArray _dst)
 {
@@ -66,26 +97,14 @@ static void zhangSuenThinning(InputArray _src, OutputArray _dst)
 {
     uchar lut1[256] = ZHANGSUEN_LUT1;
     uchar lut2[256] = ZHANGSUEN_LUT2;
-
-    Mat src, dst;
-    threshold(_src, src, 0, 1, THRESH_BINARY);
-    src.convertTo(src, CV_8UC1);
-
-    while(applyLUTNeighbors(src, dst, lut1) + applyLUTNeighbors(dst, src, lut2));
-    _dst.assign(src);
+    twoPassThinning(_src, _dst, lut1, lut2);
 }
 
 static void guoHallThinning(InputArray _src, OutputArray _dst)
 {
     uchar lut1[256] = GUOHALL_LUT1;
     uchar lut2[256] = GUOHALL_LUT2;
-
-    Mat src, dst;
-    threshold(_src, src, 0, 1, THRESH_BINARY);
-    src.convertTo(src, CV_8UC1);
-
-    while(applyLUTNeighbors(src, dst, lut1) + applyLUTNeighbors(dst, src, lut2));
-    _dst.assign(src);
+    twoPassThinning(_src, _dst, lut1, lut2);
 }
 
 void skeletonize(InputArray _src, OutputArray _dst, int type)
@@ -106,20 +125,12 @@ void skeletonize(InputArray _src, OutputArray _dst, int type)
 void skeleton::branchPoints(InputArray _src, OutputArray _dst)
 {
     uchar lut[256] = BRANCH_LUT;
-
-    Mat src;
-    threshold(_src, src, 0, 1, THRESH_BINARY);
-    src.convertTo(src, CV_8UC1);
-    applyLUTNeighbors(src, _dst, lut);
+    simpleLut(_src, _dst, lut);
 }
 
 void skeleton::endPoints(InputArray _src, OutputArray _dst)
 {
     uchar lut[256] = ENDPTS_LUT;
-
-    Mat src;
-    threshold(_src, src, 0, 1, THRESH_BINARY);
-    src.convertTo(src, CV_8UC1);
-    applyLUTNeighbors(src, _dst, lut);
+    simpleLut(_src, _dst, lut);
 }
 }
